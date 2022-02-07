be78c9ce85dc875e1fbb1188713f330024c050de
Paul de Mast <pjf.demast@avans.nl>
2022-02-07 13:05:04 +0100

    First version: demo DbContext crud operations

--- Les2EFIntro/Data/TeamManagmentDBContext.cs
+++ Les2EFIntro/Data/TeamManagmentDBContext.cs
@@ -19,20 +19,7 @@ namespace Les2EFIntro.Data
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            //modelBuilder.Entity<Member>().OwnsOne(m => m.Contact);
 
-            modelBuilder.Entity<Member>().HasData(
-                new Member { Id = 1001, Name = "Henk", Email = "henk@breda.nl", TeamId = 201 },
-                new Member { Id = 1002, Name = "Loes", Email = "loes@avans.nl", TeamId = 201 }
-                //new Member { Id = 1003, Name = "Job",  TeamId = 202, Contact= new Models.Contact { Email="job@gmail.com", Phone="765"} }
-                );
-
-            modelBuilder.Entity<Team>().HasData(
-                new Team { Id = 201, Group = "Groep-1", Title = "You snooze, you loose!" },
-                new Team { Id = 202, Group = "Groep-2", Title = "Nachos" },
-                new Team { Id = 203, Group = "Groep-3", Title = "3 front-end devs + 1 back-end dev" },
-                new Team { Id = 204, Group = "Groep-4", Title = "Cinavas" }
-                );
         }
 
     }
--- Les2EFIntro/Program.cs
+++ Les2EFIntro/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Les2EFIntro.Data;
+using Les2EFIntro.Models;
 using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Team management");
@@ -14,3 +15,36 @@ Console.WriteLine($"Team {team1.Title} has the following members:");
 foreach (var member in team1.Enrolled) {
     Console.WriteLine($"\t{member.Name}");
 }
+
+#region demo creating and adding new entities:
+teamsManagementContext.Members.Add(new Member { Name = "Jeroen", Email = "jeroen@breda.nl" });
+
+var memberList = new List<Member> {
+    new Member { Name="Henk", Email = "Henk@breda.nl"},
+    new Member { Name="Loes", Email = "Loes@avans.nl"},
+};
+
+teamsManagementContext.Members.AddRange(memberList);
+teamsManagementContext.SaveChanges();
+
+var selectedStudents = teamsManagementContext
+    .Members
+    .Where(member => member.Name.Contains("oe"))
+    .OrderBy(member => member.Name)
+    .ToList();
+
+foreach (var member in selectedStudents) {
+    Console.WriteLine(member.Name);
+}
+
+Console.WriteLine("before update");
+Member before = teamsManagementContext.Members.FirstOrDefault(Member => Member.Name == "Henk");
+Console.WriteLine(before.Email);
+
+before.Email = "henk@gmail.nl";
+teamsManagementContext.SaveChanges();
+
+Console.WriteLine("after update");
+Member after = teamsManagementContext.Members.FirstOrDefault(Member => Member.Name == "Henk");
+Console.WriteLine(after.Email);
+#endregion
